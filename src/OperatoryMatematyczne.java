27469ec786398ca506b1b353dd1d9e56d3faf012
BetiSu <zurawska.qa@gmail.com>
2024-08-24 13:33:51 +0200

    lekcja68, 67,66,65

---
+++ src/OperatoryMatematyczne.java
@@ -0,0 +1,21 @@
+public class OperatoryMatematyczne {
+
+    public static  void main(String[] args){
+    int firstNumber = 4;
+    int secondNumber = 6;
+
+
+
+    int addition = firstNumber + secondNumber;
+    int subtraction = firstNumber - secondNumber;
+    int multiplication = firstNumber * secondNumber;
+    int division = firstNumber/secondNumber;
+    int mod = firstNumber%secondNumber;
+
+    System.out.println("Wynik dodania" + addition);
+    System.out.println("Wynik Odemjmowania" + subtraction);
+    System.out.println("Wynik Mnożenie" + multiplication);
+    System.out.println("Wynik Dzielenie" + division);
+    System.out.println("Wynik Modulo" + mod);
+    }
+}
---
+++ src/typyObiektowe.java
@@ -0,0 +1,25 @@
+public class typyObiektowe {
+
+    public static void main(String[] args){
+        byte firstNumber = 127;
+        short secondNumber = 32689;
+        int thirdNumber = 32768999;
+        long fourthNumber = 2148000L;
+
+        float fifthNumber = 4.99934F; // 6-7 liczb po przecinku
+        double sixthNumber = 3.999999999999999D;// 15 cyfr po przecinku
+
+        boolean prawda = true;
+        boolean falsz = false;
+
+        char latter = 'A';
+
+
+        String hello = "hello Be";
+
+
+
+    }
+
+
+}
