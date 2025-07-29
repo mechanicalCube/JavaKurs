ffa635e2d10c92fe8991d94b586974bb4145391e
BetiSu <zurawska.qa@gmail.com>
2025-07-29 18:05:22 +0200

    lekcja 87 88 89 90

---
+++ src/AutoRef.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+public class AutoRef {
+
+    public String marka;
+    public String model;
+    public int rokProdukcji;
+    public int przebieg;
+
+    public void jedz(){
+        System.out.println("jade!");
+    }
+
+    public void hamuj(){
+        System.out.println("Hamuj!");
+    }
+
+    public void info(){
+        System.out.println("Marka: " + marka);
+        System.out.println("Model: " + model);
+        System.out.println("Rok produkcji: " + rokProdukcji);
+        System.out.println("Przebieg: " + przebieg);
+    }
+
+}
