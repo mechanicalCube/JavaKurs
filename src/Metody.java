d95fc7c4aa2b5fd0f9bf70b2b8400490d4466f0e
BetiSu <zurawska.qa@gmail.com>
2025-07-31 20:03:21 +0200

    Metody

---
+++ src/Metody.java
@@ -0,0 +1,12 @@
+public class Metody {
+
+    public void policzWynik (){
+        System.out.println("zaraz policzę wynik");
+        int result = 0;
+        for (int i =0; i<100; i++){
+            result = result +i;
+        }
+        System.out.println("rezultat to: " + result);
+    }
+
+}
