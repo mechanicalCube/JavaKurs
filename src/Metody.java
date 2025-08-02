3d70e7a2fe134e19538df542931af3bcfee70330
BetiSu <zurawska.qa@gmail.com>
2025-08-02 08:35:37 +0200

    Medoty 2

--- src/Metody.java
+++ src/Metody.java
@@ -20,4 +20,22 @@ public class Metody {
 
     }
 
+    public void policzWynikParam(int number){
+        System.out.println("number ma wartość: " + number);
+        System.out.println("zaraz policzę wynik");
+        int result = 0;
+        for (int i =0; i< number; i++){
+            result = result +i;
+        }
+        System.out.println("rezultat to: " + result);
+    }
+
+    public int add(int firstNumber, int secondNumber) {
+        System.out.println("suma to " + (firstNumber + secondNumber));
+        if(firstNumber ==0){
+            return 0;
+        }
+
+        return firstNumber + secondNumber;
+    }
 }
