4d6a1217eab0ba25658e152c8f8576685c22f712
BetiSu <zurawska.qa@gmail.com>
2025-08-17 16:55:59 +0200

    lekcja 109 Praca domowa

---
+++ src/AndroidApp.java
@@ -0,0 +1,11 @@
+public class AndroidApp extends App {
+
+
+    public AndroidApp(String name) {
+        super(name);
+    }
+
+    public void iAndroidInfo(){
+        System.out.println("uruchamiamy na Androdzie");
+    }
+}
---
+++ src/IPhoneApp.java
@@ -0,0 +1,10 @@
+public class IPhoneApp extends App  {
+
+    public IPhoneApp(String name) {
+        super(name);
+    }
+
+    public void iPhoneInfo(){
+        System.out.println("uruchamiamy na iPhonie");
+    }
+}
