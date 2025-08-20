0af77716cb3c6bf23268039d6c98ccabfc9f2271
BetiSu <zurawska.qa@gmail.com>
2025-08-20 10:06:24 +0200

    przeciązenie metod

---
+++ src/MethodNameTest.java
@@ -0,0 +1,7 @@
+public class MethodNameTest {
+    public static void main(String[] args) {
+        MethodName methodName = new MethodName();
+        methodName.add(3, 4);
+        methodName.add(4,4,4);
+    }
+}
