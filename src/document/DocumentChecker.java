7d664dce94294264cc2673f4a5eb4d8ae2860634
BetiSu <zurawska.qa@gmail.com>
2025-10-19 16:20:30 +0200

    polimorfizm

---
+++ src/document/DocumentChecker.java
@@ -0,0 +1,9 @@
+package document;
+
+public class DocumentChecker {
+    public static void main(String[] args) {
+        Document doc = new PdfDocument();
+
+        doc.getDescription();
+    }
+}
---
+++ src/document/ExcelDocument.java
@@ -0,0 +1,7 @@
+package document;
+
+public class ExcelDocument {
+    public void getDescription(){
+        System.out.println("I am exel document");
+    }
+}
