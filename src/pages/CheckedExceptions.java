d5904895dd7cf72c31e3a48cc003fea54d7cbb34
BetiSu <zurawska.qa@gmail.com>
2025-10-26 16:12:27 +0100

    Wyjątki cd

--- src/pages/CheckedExceptions.java
+++ src/pages/CheckedExceptions.java
@@ -8,12 +8,22 @@ import java.io.FileNotFoundException;
 public class CheckedExceptions {
 
 
-    public static void main(String[] args) throws FileNotFoundException {
-        readFile("/scr/dfdasf.pl");
-    }
+    public static void main(String[] args) {
 
-    public static void readFile(String path) throws FileNotFoundException {
-        FileInputStream fileInputStream = new FileInputStream(path);
 
+        try {
+            System.out.println("Before reading file");
+            readFile("D:\\Projekty\\JavaKurs\\src\\pages\\tests.txt");
+            System.out.println("close file");
+        } catch (FileNotFoundException e) {
+            System.out.println("Wyjątek został wyrzucony");
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("closing file");
+        }
     }
-}
+        public static void readFile (String path) throws FileNotFoundException{
+            FileInputStream fileInputStream = new FileInputStream(path);
+
+        }
+    }
