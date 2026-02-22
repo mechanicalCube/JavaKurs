2c9464428553f0e95e10edec5b949b382120d559
BetiSu <zurawska.qa@gmail.com>
2026-02-22 16:24:27 +0100

    zadanie 2

---
+++ src/Interview/Palindrom.java
@@ -0,0 +1,20 @@
+package Interview;
+
+public class Palindrom {
+
+    public static void main(String[] args) {
+        System.out.println(isPallindrom("jaj"));
+    }
+
+    public static boolean isPallindrom(String word) {
+        int n = word.length();
+        for (int i = 0; i < (n / 2); i++) {
+            if (word.charAt(i) != word.charAt(n - i - 1)) {;
+            return false;
+            }
+        }
+        return true;
+    }
+
+
+}
